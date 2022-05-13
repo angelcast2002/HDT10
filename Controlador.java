1be73279a190db9588705cc6d78777ee59560b1a
Javier Alejandro Ramírez Heredia <ram21600@uvg.edu.gt>
2022-05-13 13:18:11 -0600

    Update Controlador.java

---
+++ Controlador.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Scanner;
