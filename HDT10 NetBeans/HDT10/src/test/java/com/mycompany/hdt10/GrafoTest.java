b32807ac3c4181588d1720c7f913cae61ebcff87
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-05-18 16:08:59 -0600

    Update GrafoTest.java
    Agrega mas Junits

--- HDT10 NetBeans/HDT10/src/test/java/com/mycompany/hdt10/GrafoTest.java
+++ HDT10 NetBeans/HDT10/src/test/java/com/mycompany/hdt10/GrafoTest.java
@@ -11,131 +11,64 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
+import org.junit.Test;
+
+
 /**
  *
  * @author Angel
  */
 public class GrafoTest {
     
-    public GrafoTest() {
-    }
     
-    @BeforeClass
-    public static void setUpClass() {
+
+
+    Controlador controlador = new Controlador(null);
+
+    @Test
+    public void agregarArco() {
+        Grafo grafo = new Grafo(5);
+
+        try {
+            grafo.ingresarArco(0,0,5);
+            assertEquals(true, true);
+
+        } catch (Exception e) {
+            assertEquals(true, false);
+        }
     }
+
+
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    @Test
+    public void agregarNodo() {
+        Grafo grafo = new Grafo(5);
+
+        try {
+            grafo.ingresarNombre(0,"Ciudad ejemplo");
+            assertEquals(true, true);
+
+        } catch (Exception e) {
+            assertEquals(true, false);
+        }
+
+
     }
 
-    /**
-     * Test of ingresarArco method, of class Grafo.
-     */
     @Test
-    public void testIngresarArco() {
-        System.out.println("ingresarArco");
-        int n1 = 0;
-        int n2 = 0;
-        int peso = 0;
-        Grafo instance = null;
-        instance.ingresarArco(n1, n2, peso);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void algoritmoFloyd() {
+        Grafo grafo = new Grafo(5);
+
+        try {
+            assertEquals(null, grafo.centro());
+
+        } catch (Exception e) {
+            assertEquals(true, false);
+        }
     }
 
-    /**
-     * Test of ingresarNombre method, of class Grafo.
-     */
-    @Test
-    public void testIngresarNombre() {
-        System.out.println("ingresarNombre");
-        int nodo = 0;
-        String nombre = "";
-        Grafo instance = null;
-        instance.ingresarNombre(nodo, nombre);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of centro method, of class Grafo.
-     */
-    @Test
-    public void testCentro() {
-        System.out.println("---centro---");
-        Grafo instance = null;
-        String expResult = "Antigua";
-        String result = instance.centro();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of calcular method, of class Grafo.
-     */
-    @Test
-    public void testCalcular() {
-        System.out.println("calcular");
-        Grafo instance = null;
-        instance.calcular();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of pesominimo method, of class Grafo.
-     */
-    @Test
-    public void testPesominimo() {
-        System.out.println("pesominimo");
-        int org = 0;
-        int des = 0;
-        Grafo instance = null;
-        int expResult = 0;
-        int result = instance.pesominimo(org, des);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of caminocorto method, of class Grafo.
-     */
-    @Test
-    public void testCaminocorto() {
-        System.out.println("caminocorto");
-        int org = 0;
-        int des = 0;
-        Grafo instance = null;
-        String expResult = "";
-        String result = instance.caminocorto(org, des);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getNombre method, of class Grafo.
-     */
-    @Test
-    public void testGetNombre() {
-        System.out.println("getNombre");
-        int nodo = 0;
-        Grafo instance = null;
-        String expResult = "";
-        String result = instance.getNombre(nodo);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
+    
+
